33cada04ff1ab22604ed7685e364317778be6474
Nezdanchick <win10artem@gmail.com>
2023-02-12 00:50:18 +0400

    fixed run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,17 @@ ROOT=$(realpath -s "./")
 SCRIPTS=./scripts
 BUILD=build.sh
 RUN=qemu.sh
+FILE=$1
 
 mkdir -p -v $ROOT/bin/
 cd $SCRIPTS
-if bash $BUILD $1 ; then
-    bash $RUN $1
+
+if [$FILE == ""] ; then
+    FILE="boot"
+fi
+
+if bash $BUILD $FILE ; then
+    bash $RUN $FILE
 else
     read -p "Press Enter to continue..."
 fi
