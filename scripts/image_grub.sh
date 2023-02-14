fd3b5046adc11352a29596348e3295325adff9db
Nezdanchick <win10artem@gmail.com>
2023-02-14 19:48:44 +0400

    delete sudo

--- scripts/image_grub.sh
+++ scripts/image_grub.sh
@@ -5,7 +5,7 @@ LOADER=$BIN/kernel.bin
 IMG=../img
 
 mkdir -p $BIN
-sudo cp ../config/grub.cfg $IMG/boot/grub
+cp ../config/grub.cfg $IMG/boot/grub
 
-sudo cp $BIN/kernel.elf $IMG/boot/kernel
+cp $BIN/kernel.elf $IMG/boot/kernel
 grub-mkrescue --output=$BIN/image.img $IMG
