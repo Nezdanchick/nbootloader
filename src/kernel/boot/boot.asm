05ee13241ffd96d293a338200053ff13f3bee04c
Nezdanchick <win10artem@gmail.com>
2023-05-11 01:17:27 +0400

    boot rework started

--- src/kernel/boot/boot.asm
+++ src/kernel/boot/boot.asm
@@ -1,8 +1,12 @@
 bits 32
 extern main
 
-%include "boot/multiboot2.inc"
+%include "boot/longmode.inc"
+%include "boot/gdt.inc"
 
+%include "boot/multiboot2.inc" ; entry
+call gdt
+call cpuid
 mov esp, 0x02000000 ;Setup the stack at 2MiB
 push 0              ;Reset EFLAGS
 popfd
@@ -14,7 +18,6 @@ in al, 0x92
 or al, 2
 out 0x92, al
 
-lgdt [gdtr]
 
 xor eax, eax
 mov ebx, eax
@@ -25,13 +28,3 @@ call main
 add esp, 8          ;Cleanup 8 bytes pushed as arguments
 hlt
 
-; GDT
-gdt:
-  db 0x00, 0x00, 0x00, 0x00, 0x00,      0x00,      0x00, 0x00 
-  db 0xFF, 0xFF, 0x00, 0x00, 0x00, 10011010b, 11001111b, 0x00
-gdt_size  equ $ - gdt
-
-;GDTR data
-gdtr:
-  dw gdt_size - 1
-  dd gdt
